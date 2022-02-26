3d47c488e13a40ffdada03e49b6a17d75a7d0061
linuxjefeprive <92636137+linuxjefeprive@users.noreply.github.com>
2022-02-26 16:53:52 +0100

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -1,3 +1,4 @@
+// This is where we put the job we want auto-insterted into Jenkins, We can build as much groovy jobs as we want to insert any job automatically. In this case it's just Hello World.
 def githubUrl = "https://github.com/linuxjefeprive/Hello-World-Code.git"
 
 pipelineJob("DevOps WAR File Project") {
